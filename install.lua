9abff0778707629a5f6912d7aa51e95909e9a607
ItsSurface <lino.vs@rogers.com>
2024-05-09 19:40:56 -0400

    Changed install system

--- install.lua
+++ install.lua
@@ -1,11 +1,10 @@
 args = {...}
-mydir = "/sfcc"
-github = "https://raw.githubusercontent.com/ItsSurface/SFCC/main/"
 
 function download(ghpath, path)
     shell.run("wget", github .. ghpath .. ".lua", mydir .. path)
 end
 
-fs.delete(mydir)
-fs.makeDir(mydir)
-download("testmine", "/testmine")
+fs.delete("/update")
+download("update", "/update")
+
+shell.run("/update")
---
+++ update.lua
@@ -0,0 +1,10 @@
+mydir = "/sfcc"
+github = "https://raw.githubusercontent.com/ItsSurface/SFCC/main/"
+
+function download(ghpath, path)
+    shell.run("wget", github .. ghpath .. ".lua", mydir .. path)
+end
+
+fs.delete(mydir)
+fs.makeDir(mydir)
+download("mine/test", "/mine/test")
