3cca14c74cd02288b5f3fe6784001dfe3b0f64f8
Surface <86692232+ItsSurface@users.noreply.github.com>
2024-05-11 19:52:11 -0400

    Fixed inventory filling bug again

--- mine/harvest.lua
+++ mine/harvest.lua
@@ -18,7 +18,7 @@ nonSeedPlantables = {
 function seed()
     for i = 16, 1, -1 do
         local details = turtle.getItemDetail(i, true)
-        if turtle.getItemCount(i) > 0 and (details.tags["c:seeds"] or nonSeedPlantables[details.name]) then
+        if details.count > 0 and (details.tags["c:seeds"] or nonSeedPlantables[details.name]) then
             turtle.select(i)
             turtle.placeDown()
             if ejectSeeds and details.tags["c:seeds"] and details.count > 55 then
@@ -26,6 +26,9 @@ function seed()
                 turtle.select(1)
             elseif i ~= 16 then
                 turtle.transferTo(16)
+            end
+            if details.count == 1 then
+                turtle.select(1)
             end
             return
         end
