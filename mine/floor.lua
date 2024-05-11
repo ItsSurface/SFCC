3a840eaa6e7c365b83df3c4b433a8bd60957f8b8
Surface <86692232+ItsSurface@users.noreply.github.com>
2024-05-11 00:51:07 -0400

    Added check for block type to floor script

--- mine/floor.lua
+++ mine/floor.lua
@@ -6,6 +6,13 @@ end
 
 length = tonumber(args[1])-1
 width = tonumber(args[2])
+-- get the block in the first slot of the inventory, if there is none then print an error message, otherwise store the type of block in a variable and ensure that every block placed is the same type
+turtle.select(1)
+if turtle.getItemCount(1) == 0 then
+    print("No blocks to place")
+    return
+end
+blockType = turtle.getItemDetail(1).name
 
 function forward()
     while not turtle.forward() do
@@ -16,7 +23,7 @@ end
 function placeInvDown()
     for i = 1, 16 do
         turtle.select(i)
-        if turtle.getItemCount(i) > 0 then
+        if turtle.getItemCount(i) > 0 and turtle.getItemDetail(i).name == blockType then
             turtle.placeDown()
             return
         end
